d3ce6e0fa10d6e60108c887110c2ab73c868e052
rasnes <rasmusnes@gmail.com>
2024-10-15 23:25:59 +0200

    refactor(dashboard): moved function to duck.py

--- dashboard/src/dashboard.py
+++ dashboard/src/dashboard.py
@@ -3,15 +3,15 @@ import datetime
 import streamlit as st
 import altair as alt
 
-from utils.duck import Daily, ibis_con
+import utils.duck as duck
 
 st.set_page_config(layout="wide")
 st.title("Dashboard")
 
-t = ibis_con.table("daily_adjusted")
+t = duck.ibis_con.table("daily_adjusted")
 st.table(t.limit(5).execute())
 
-daily = Daily(t)
+daily = duck.Daily(t)
 
 # Cache all_tickers
 @st.cache_data
@@ -19,16 +19,6 @@ def get_all_tickers():
     return daily.get_tickers().execute()
 
 all_tickers = get_all_tickers()
-
-def display_chart(selected_tickers, date_from, date_to) -> None:
-    c = (
-        alt.Chart(
-            daily.get_relative(selected_tickers, str(date_from), str(date_to)).execute()
-        )
-        .mark_line()
-        .encode(x="date", y="relative", color="ticker")
-    )
-    st.altair_chart(c, use_container_width=True)
 
 date_from = st.date_input("From", value=datetime.date(2024, 1, 1))
 date_to = st.date_input("To", value=datetime.datetime.now())
@@ -42,4 +32,4 @@ selected_tickers = st.multiselect(
 )
 
 # Display the chart using the selected tickers
-display_chart(selected_tickers, date_from, date_to)
+duck.relative_chart(daily, selected_tickers, date_from, date_to)
--- dashboard/src/utils/duck.py
+++ dashboard/src/utils/duck.py
@@ -2,6 +2,7 @@ from os import environ
 import duckdb
 import ibis
 import streamlit as st
+import altair as alt
 
 motherduck_creds = (
     f"md:{environ['APP_ENV']}?motherduck_token={environ['MOTHERDUCK_TOKEN']}"
@@ -21,6 +22,16 @@ class Daily:
     def get_relative(
         self, tickers: list[str], start_date: str, end_date: str
     ) -> ibis.Expr:
+        """Calculates the relative price of a stock over a given time period.
+
+        Args:
+            tickers: A list of tickers to calculate the relative price for.
+            start_date: The start date of the time period.
+            end_date: The end date of the time period.
+
+        Returns:
+            An ibis expression representing the relative price of the stock.
+        """
         # Subquery to perform the aggregation
         t_agg: ibis.Expr = (
             self.t.select("ticker", "date", "adjClose")
@@ -40,3 +51,14 @@ class Daily:
                 ibis.window(group_by="ticker", order_by="date")
             )  # type: ignore
         )
+
+
+def relative_chart(daily: Daily, selected_tickers, date_from, date_to) -> None:
+    c = (
+        alt.Chart(
+            daily.get_relative(selected_tickers, str(date_from), str(date_to)).execute()
+        )
+        .mark_line()
+        .encode(x="date", y="relative", color="ticker")
+    )
+    st.altair_chart(c, use_container_width=True)
