1bf592d8ae1284385a380044663772fd73451790
rasnes <rasmusnes@gmail.com>
2024-11-03 13:24:13 +0100

    refactor(EtL): created Pipeline type and made pipeline functions to methods instead

--- EtL/pipeline/end_of_day.go
+++ EtL/pipeline/end_of_day.go
@@ -1,27 +1,48 @@
 package pipeline
 
 import (
+	"archive/zip"
 	"errors"
 	"fmt"
+	"log/slog"
+
 	"github.com/rasnes/tiingo-duckdb-framework/EtL/config"
 	"github.com/rasnes/tiingo-duckdb-framework/EtL/extract"
 	"github.com/rasnes/tiingo-duckdb-framework/EtL/load"
-	"log/slog"
 )
 
-func DailyEndOfDay(config *config.Config, logger *slog.Logger) (int, error) {
+type Pipeline struct {
+	DuckDB       *load.DuckDB
+	TiingoClient *extract.TiingoClient
+	Logger       *slog.Logger
+}
+
+func NewPipeline(config *config.Config, logger *slog.Logger) (*Pipeline, error) {
 	db, err := load.NewDuckDB(config, logger)
 	if err != nil {
-		return 0, fmt.Errorf("error creating DB database: %v", err)
+		return nil, fmt.Errorf("error creating DB database: %v", err)
 	}
-	defer db.Close()
 
 	httpClient, err := extract.NewTiingoClient(config, logger)
 	if err != nil {
-		return 0, fmt.Errorf("error creating HTTP client: %v", err)
+		return nil, fmt.Errorf("error creating Tiingo HTTP client: %v", err)
 	}
 
-	zipSupportedTickers, err := httpClient.GetSupportedTickers()
+	return &Pipeline{
+		DuckDB:       db,
+		TiingoClient: httpClient,
+		Logger:       logger,
+	}, nil
+}
+
+func (p *Pipeline) Close() {
+	p.DuckDB.Close()
+}
+
+func (p *Pipeline) DailyEndOfDay() (int, error) {
+	defer p.Close()
+
+	zipSupportedTickers, err := p.TiingoClient.GetSupportedTickers()
 	if err != nil {
 		return 0, fmt.Errorf("error getting supported_tickers.csv.zip: %v", err)
 	}
@@ -31,24 +52,24 @@ func DailyEndOfDay(config *config.Config, logger *slog.Logger) (int, error) {
 		return 0, fmt.Errorf("error unzipping supported_tickers.csv.zip: %v", err)
 	}
 
-	if err := db.LoadCSV(csvSupportedTickers, "supported_tickers", false); err != nil {
+	if err := p.DuckDB.LoadCSV(csvSupportedTickers, "supported_tickers", false); err != nil {
 		return 0, fmt.Errorf("error loading supported_tickers.csv into DB: %v", err)
 	}
 
-	lastTradingDay, err := httpClient.GetLastTradingDay()
+	lastTradingDay, err := p.TiingoClient.GetLastTradingDay()
 	if err != nil {
 		return 0, fmt.Errorf("error getting ticker data from last trading day: %v", err)
 	}
 
-	if err := db.LoadCSV(lastTradingDay, "last_trading_day", false); err != nil {
+	if err := p.DuckDB.LoadCSV(lastTradingDay, "last_trading_day", false); err != nil {
 		return 0, fmt.Errorf("error loading last_trading_day into DB: %v", err)
 	}
 
-	if err := db.RunQueryFile("../sql/insert__last_trading_day.sql"); err != nil {
+	if err := p.DuckDB.RunQueryFile("../sql/insert__last_trading_day.sql"); err != nil {
 		return 0, fmt.Errorf("error inserting last trading day into daily_adjusted: %v", err)
 	}
 
-	res, err := db.GetQueryResultsFromFile("../sql/query__selected_backfill.sql")
+	res, err := p.DuckDB.GetQueryResultsFromFile("../sql/query__selected_backfill.sql")
 	if err != nil {
 		return 0, fmt.Errorf("error getting backfill results: %v", err)
 	}
@@ -61,7 +82,7 @@ func DailyEndOfDay(config *config.Config, logger *slog.Logger) (int, error) {
 		return 0, nil
 	}
 
-	nTickers, err := BackfillEndOfDay(tickers, httpClient, logger, db)
+	nTickers, err := p.BackfillEndOfDay(tickers)
 	if err != nil {
 		return nTickers, fmt.Errorf("error backfilling tickers: %v", err)
 	}
@@ -69,21 +90,10 @@ func DailyEndOfDay(config *config.Config, logger *slog.Logger) (int, error) {
 	return len(tickers), nil
 }
 
-// historyFetcher defines the interface for fetching historical data.
-type historyFetcher interface {
-	GetHistory(ticker string) ([]byte, error)
-}
-
-// csvLoader defines the interface for loading CSV data.
-type csvLoader interface {
-	LoadCSV(csv []byte, table string, insert bool) error
-}
-
-// BackfillEndOfDay fetches historical EoD prices for a list of tickers and loads them into the database
-func BackfillEndOfDay(tickers []string, client historyFetcher, logger *slog.Logger, db csvLoader) (int, error) {
+func (p *Pipeline) BackfillEndOfDay(tickers []string) (int, error) {
 	var errorList []error
 	for i, ticker := range tickers {
-		history, err := client.GetHistory(ticker)
+		history, err := p.TiingoClient.GetHistory(ticker)
 		if err != nil {
 			errorList = append(errorList, fmt.Errorf("error fetching history for ticker %s: %w", ticker, err))
 			continue
@@ -95,17 +105,16 @@ func BackfillEndOfDay(tickers []string, client historyFetcher, logger *slog.Logg
 			continue
 		}
 
-		if err := db.LoadCSV(historyWithTicker, "daily_adjusted", true); err != nil {
+		if err := p.DuckDB.LoadCSV(historyWithTicker, "daily_adjusted", true); err != nil {
 			errorList = append(errorList, fmt.Errorf("error loading history to DB for ticker %s: %w", ticker, err))
 			continue
 		}
 
 		if i > 0 && i%20 == 0 {
 			if len(errorList) > 0 {
-				nErrors := len(errorList)
-				logger.Info(fmt.Sprintf("Successfully backfilled %d tickers; failed on %d tickers", i-nErrors, nErrors))
+				p.Logger.Info(fmt.Sprintf("Successfully backfilled %d tickers; failed on %d tickers", i-len(errorList), len(errorList)))
 			} else {
-				logger.Info(fmt.Sprintf("Successfully backfilled %d tickers", i))
+				p.Logger.Info(fmt.Sprintf("Successfully backfilled %d tickers", i))
 			}
 		}
 	}
