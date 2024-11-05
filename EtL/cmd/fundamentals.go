8699bddd76243634b19b8ec25d9e629ce7c63173
rasnes <rasmusnes@gmail.com>
2024-11-05 15:50:11 +0100

    feat(CLI): added statements

--- EtL/cmd/fundamentals.go
+++ EtL/cmd/fundamentals.go
@@ -79,7 +79,46 @@ func newMetadataCmd() *cobra.Command {
 	}
 }
 
+func newStatementsCmd() *cobra.Command {
+    var tickers string
+
+    cmd := &cobra.Command{
+        Use:   "statements [--tickers TICKER1,TICKER2,...]",
+        Short: "Updates financial statements data for selected tickers",
+        RunE: func(cmd *cobra.Command, args []string) error {
+            cfg, log, err := initializeConfigAndLogger()
+            if err != nil {
+                return err
+            }
+
+            pipeline, err := pipeline.NewPipeline(cfg, log)
+            if err != nil {
+                return fmt.Errorf("error creating pipeline: %w", err)
+            }
+            defer pipeline.Close()
+
+            var tickerSlice []string
+            if tickers != "" {
+                tickerSlice = strings.Split(tickers, ",")
+            }
+
+            rowsAffected, err := pipeline.Statements(tickerSlice)
+            if err != nil {
+                return fmt.Errorf("error updating statements: %w", err)
+            }
+
+            log.Info(fmt.Sprintf("Successfully updated statements for %d tickers", rowsAffected))
+
+            return nil
+        },
+    }
+
+    cmd.Flags().StringVar(&tickers, "tickers", "", "Comma-separated list of tickers (e.g., AAPL,MSFT,GOOGL)")
+    return cmd
+}
+
 func init() {
 	fundamentalsCmd.AddCommand(newMetadataCmd())
 	fundamentalsCmd.AddCommand(newFundamentalsDailyCmd())
+	fundamentalsCmd.AddCommand(newStatementsCmd())
 }
