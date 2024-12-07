4272875e56e89f6093f71008ff50ca301f47202f
rasnes <rasmusnes@gmail.com>
2024-12-07 20:49:54 +0100

    fix(transformations): switched to using only a single index for comparison, SPY

--- transformations/src/sql/macros.sql
+++ transformations/src/sql/macros.sql
@@ -8,66 +8,41 @@ end;
 -- Updated macro with exchange logic built in
 create or replace macro index_volatility(
     price,
-    qqq_price,
-    spy_price,
-    exchange_val,
+    index_price,
     partition_col,
     date_col,
     periods
 ) as
 case
-    when exchange_val = 'NASDAQ'
-        then case
-            when row_number() over (partition by partition_col order by date_col) > periods
-            then stddev(price/qqq_price) over (partition by partition_col order by date_col rows between periods preceding and current row)
-            else null
-        end
-    else
-        case
-            when row_number() over (partition by partition_col order by date_col) > periods
-            then stddev(price/spy_price) over (partition by partition_col order by date_col rows between periods preceding and current row)
-            else null
-        end
+    when row_number() over (partition by partition_col order by date_col) > periods
+    then stddev(price/index_price) over (partition by partition_col order by date_col rows between periods preceding and current row)
+    else null
 end;
 
 create or replace macro future_return(price_col, partition_col, date_col, periods) as
     lead(price_col, periods) over (partition by partition_col order by date_col) / nullif(price_col, 0);
 
 -- Macro for excess returns calculation
-create or replace macro excess_return(stock_return, qqq_return, spy_return, exchange_val) as
+create or replace macro excess_return(stock_return, index_return) as
 case
-    when exchange_val = 'NASDAQ' and qqq_return > 0 and stock_return > 0
-        then ln(nullif(stock_return / qqq_return, 0))
-    when exchange_val != 'NASDAQ' and spy_return > 0 and stock_return > 0
-        then ln(nullif(stock_return / spy_return, 0))
+    when index_return > 0 and stock_return > 0
+        then ln(nullif(stock_return / index_return, 0))
     else null
 end;
 
 -- Macro for relative SMA development
 create or replace macro relative_sma_development(
     stock_sma,
-    qqq_sma,
-    spy_sma,
-    exchange_val,
+    index_sma,
     partition_col,
     date_col
 ) as
-case
-    when exchange_val = 'NASDAQ'
-        then nullif(
-            (stock_sma / lag(stock_sma, 1) over (partition by partition_col order by date_col)) /
-            (qqq_sma / lag(qqq_sma, 1) over (order by date_col)),
-            0)
-    when exchange_val != 'NASDAQ'
-        then nullif(
-            (stock_sma / lag(stock_sma, 1) over (partition by partition_col order by date_col)) /
-            (spy_sma / lag(spy_sma, 1) over (order by date_col)),
-            0)
-    else null
-end;
+nullif(
+    (stock_sma / lag(stock_sma, 1) over (partition by partition_col order by date_col)) /
+    (index_sma / lag(index_sma, 1) over (order by date_col)),
+    0);
 
--- TODO: consider making values to float to save space
--- TODO: double check primary key and or index
+
 create table if not exists main.predictions (
     date DATE,
     ticker VARCHAR,
